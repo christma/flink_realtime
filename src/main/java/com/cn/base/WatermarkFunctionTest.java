c97f207173dfa3eb2312b4b7f6f9430435f5a071
christma <liuchuanmumumu@gmail.com>
2023-03-24 17:07:38 +0800

    add methods

---
+++ src/main/java/com/cn/base/WatermarkFunctionTest.java
@@ -0,0 +1,31 @@
+package com.cn.base;
+
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.time.Duration;
+
+public class WatermarkFunctionTest {
+
+    public static void main(String[] args) throws Exception {
+
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.addSource(new ClickSource())
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ZERO)
+                                .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                                    @Override
+                                    public long extractTimestamp(Event event, long l) {
+                                        return event.timestamp;
+                                    }
+                                })
+                ).print();
+
+
+        env.execute();
+
+    }
+}
