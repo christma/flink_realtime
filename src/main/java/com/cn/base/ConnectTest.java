48eefbdcfacd7505aa21208cc40ab95e8b48c07a
christma <liuchuanmumumu@gmail.com>
2023-04-03 16:13:29 +0800

    add methods

--- src/main/java/com/cn/base/ConnectTest.java
+++ src/main/java/com/cn/base/ConnectTest.java
@@ -2,21 +2,15 @@ package com.cn.base;
 
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
-import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
-import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.api.functions.co.CoProcessFunction;
 import org.apache.flink.util.Collector;
 
-
-import java.sql.Timestamp;
 import java.time.Duration;
 
-public class UnionTest {
+public class ConnectTest {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
@@ -29,7 +23,7 @@ public class UnionTest {
                             }
                         })
                 );
-        source1.map(x -> new Timestamp(x.timestamp)).print("source1:");
+        source1.map(x -> x.timestamp).print("source1:");
 
         SingleOutputStreamOperator<Event> source2 = env.addSource(new ClickSource2())
                 .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofSeconds(5))
@@ -40,17 +34,18 @@ public class UnionTest {
                             }
                         })
                 );
-        source2.map(x -> new Timestamp(x.timestamp)).print("source2:");
-        source1.union(source2)
-                .keyBy(data -> data.user)
-                .window(SlidingEventTimeWindows.of(Time.seconds(5), Time.seconds(5)))
-                .process(new ProcessWindowFunction<Event, String, String, TimeWindow>() {
-                    @Override
-                    public void process(String s, ProcessWindowFunction<Event, String, String, TimeWindow>.Context context, Iterable<Event> elements, Collector<String> out) throws Exception {
-                        out.collect("水位线： " + new Timestamp(context.currentWatermark()) + "  值为 " + elements.iterator().next().user);
+        source2.map(x -> x.timestamp).print("source2:");
+        source1.connect(source2).process(new CoProcessFunction<Event, Event, String>() {
+            @Override
+            public void processElement1(Event value, CoProcessFunction<Event, Event, String>.Context ctx, Collector<String> out) throws Exception {
+                out.collect("水位线1： " + ctx.timerService().currentWatermark() + "  值为 " + value.user);
+            }
 
-                    }
-                }).print();
+            @Override
+            public void processElement2(Event value, CoProcessFunction<Event, Event, String>.Context ctx, Collector<String> out) throws Exception {
+                out.collect("水位线2： " + ctx.timerService().currentWatermark() + "  值为 " + value.user);
+            }
+        }).print();
 
 
         env.execute();
