efcc179fcbfb5166863f29a8356a84c463b299b1
christma <liuchuanmumumu@gmail.com>
2023-04-08 22:46:21 +0800

    add methods

---
+++ src/main/java/com/cn/base/CoGroupTest.java
@@ -0,0 +1,57 @@
+package com.cn.base;
+
+
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.CoGroupFunction;
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.time.Duration;
+
+public class CoGroupTest {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        SingleOutputStreamOperator<Event> s1 = env.addSource(new ClickSource())
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ZERO)
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                            @Override
+                            public long extractTimestamp(Event element, long recordTimestamp) {
+                                return element.timestamp;
+                            }
+                        })
+                );
+        SingleOutputStreamOperator<Event> s2 = env.addSource(new ClickSource2())
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ZERO)
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                            @Override
+                            public long extractTimestamp(Event element, long recordTimestamp) {
+                                return element.timestamp;
+                            }
+                        })
+                );
+
+
+        s1.coGroup(s2)
+                .where(data -> data.user)
+                .equalTo(data -> data.user)
+                .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+                .apply(new CoGroupFunction<Event, Event, String>() {
+                    @Override
+                    public void coGroup(Iterable<Event> first, Iterable<Event> second, Collector<String> out) throws Exception {
+                        out.collect(first + "--> " + second);
+                    }
+                }).print();
+
+
+        env.execute();
+
+
+    }
+}
